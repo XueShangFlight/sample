5dd44b81d8bdfb754e65ed53c9804c9527f53354
zhoucong <zhoucong@inkanshop.com>
2019-06-03 08:39:59 +0000

    Finsh layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>@yield('title','Sample App') - zhoucong study Laravel Today!</title>
+	<title>@yield('title','Sample App') - Laravel 入门教程</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+	@include('layouts._header')
+
+	<div class="container">
+		@yield('content')	
+		@include('layouts._footer')
+	</div>
 </body>
 </html>
