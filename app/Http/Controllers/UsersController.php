2f20f8622aae6e6184e6da08f1d5733a79981e86
zhoucong <zhoucong@inkanshop.com>
2019-06-05 11:43:36 +0000

    Finish user edit, update,index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,15 +8,37 @@ use Auth;
 
 class UsersController extends Controller
 {
+	public function __construct(){
+
+		$this->middleware('auth',[
+			'except' => ['show','create','store','index']
+		]);
+
+		$this->middleware('guest',[
+			'only' => ['create']
+		]);
+
+	}
+
+	 public function index(){
+
+    	$users = User::paginate(10);
+    	// $users = User::all();
+    	return view('users.index',compact('users'));
+    }
+
     public function create(){
+
     	return view('users.create');
     }
 
     public function show(User $user){
+
     	return view('users.show',compact('user'));
     }
 
     public function store(Request $request){
+
     	$this->validate($request,[
     		'name' => 'required|max:50',
     		'email' => 'required|email|unique:users|max:255',
@@ -34,4 +56,42 @@ class UsersController extends Controller
 
     	return redirect()->route('users.show',[$user]);
     }
+
+    public function edit(User $user){
+
+    	$this->authorize('update',$user);
+    	return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request){
+
+    	$this->validate($request,[
+    		'name' => 'required|max:50',
+    		'password' => 'nullable|confirmed|min:6'
+    	]);
+
+    	$this->authorize('update',$user);
+
+    	$data = [];
+    	$data['name'] = $request->name;
+    	if ($request->password) {
+    		$data['password'] = bcrypt($request->password);
+    	}
+    	$user->update($data);
+
+    	session()->flash('success','个人资料更新成功!');
+    	// $user->update([
+    	// 	'name' => $request->name,
+    	// 	'password' => bcrypt($request->password),
+    	// ]);
+
+    	return redirect()->route('users.show',$user->id);
+    }
+
+   	public function destroy(User $user){
+   		$this->authorize('destroy',$user);
+   		$user->delete();
+   		session()->flash('success','成功删除用户!');
+   		return back();
+   	}
 }
